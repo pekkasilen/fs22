978b1d0f6b1d379fff7649661bbfda18e352890d
pekkasilen <50104239+pekkasilen@users.noreply.github.com>
2022-05-24 23:07:21 +0300

    Add files via upload

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,93 @@
+import { useState } from "react";
+
+const FilterForm = ({ handler }) => {
+  return (
+    <div>
+      Filter shown with <input onChange={handler} />
+    </div>
+  );
+};
+
+const PersonForm = (props) => {
+  return (
+    <div>
+      <form onSubmit={props.savePerson}>
+        <div>
+          name: <input onChange={props.handleNameChange} />
+        </div>
+        <div>
+          number: <input onChange={props.handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+const Persons = (props) => {
+  return props.persons
+    .filter((person) =>
+      person["name"].toLowerCase().includes(props.filterPhrase.toLowerCase())
+    )
+    .map((person) => (
+      <p>
+        {person.name}, {person.number}
+      </p>
+    ));
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "09123456" },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filterPhrase, setFilterPhrase] = useState("");
+
+  const savePerson = (event) => {
+    event.preventDefault();
+    if (persons.some((person) => person["name"] === newName)) {
+      alert(`${newName} already exists`);
+    } else {
+      let personObject = {
+        name: newName,
+        number: newNumber,
+      };
+      setPersons(persons.concat(personObject));
+    }
+  };
+
+  const handleNameChange = (event) => {
+    event.preventDefault();
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    event.preventDefault();
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    event.preventDefault();
+    setFilterPhrase(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <FilterForm handler={handleFilterChange} />
+
+      <PersonForm
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        savePerson={savePerson}
+      />
+      <h2>Numbers</h2>
+      <Persons persons={persons} filterPhrase={filterPhrase} />
+    </div>
+  );
+};
+
+export default App;
